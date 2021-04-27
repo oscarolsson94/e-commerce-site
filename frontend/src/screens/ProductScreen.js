1a27f3ab9d2551edf023b3c4810e8f99eb3c1b8a
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-27 10:47:44 +0200

    adding price, item status and add to cart

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -34,7 +34,25 @@ const ProductScreen = (props) => {
                     </ul>
                 </div>
                 <div className="col-1">
-                    
+                    <div className="card card-body">
+                        <ul>
+                            <li>
+                                <div className="row">
+                                    <div>Price</div>
+                                    <div className="price" >${product.price}</div>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="row">
+                                    <div>Status</div>
+                                    <div >{product.countInStock > 0 ? (<span className="success" >In Stock</span>) : (<span className="error">Unavailable</span>) }</div>
+                                </div>
+                            </li>
+                            <li>
+                                <button className="primary block">Add to Cart</button>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
