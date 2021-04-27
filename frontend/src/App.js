cb489ae3f334de8031bca5e4438be2c497faebf1
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-27 08:58:40 +0200

    adding font awesome for stars and change links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,11 @@ function App() {
     <div className="grid-container">
         <header className="row" >
             <div>
-                <a className="brand" href="index.html">Ecommerce</a>
+                <a className="brand" href="/">Ecommerce</a>
             </div>
             <div>
-                <a href="cart.html">Cart</a>
-                <a href="signin.html">Sign In</a>
+                <a href="/cart">Cart</a>
+                <a href="/signin">Sign In</a>
             </div>    
         </header>
 
