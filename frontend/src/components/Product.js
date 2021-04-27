f5c5e5f561b7efa65e74415eaa4b926e23e309f4
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-27 09:19:16 +0200

    extracting and building rating component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Rating from './Rating';
 
 const Product = ({product}) => {
     return (
@@ -10,13 +11,7 @@ const Product = ({product}) => {
                 <a href={`/product/${product._id}`}>
                     <h2>{product.name}</h2>
                 </a>
-                <div className="rating">
-                    <span><i className="fa fa-star"></i></span>
-                    <span><i className="fa fa-star"></i></span>
-                    <span><i className="fa fa-star"></i></span>
-                    <span><i className="fa fa-star"></i></span>
-                    <span><i className="fa fa-star"></i></span>
-                </div>
+                <Rating rating={product.rating} numReviews={product.numReviews} />
                 <div className="price">
                     ${product.price}
                 </div>
