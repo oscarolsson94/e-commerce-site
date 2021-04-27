2642936194a379f83024e482b12ef1cb4abf37f3
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-27 10:04:59 +0200

    adding the number of riviews

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -8,6 +8,7 @@ const Rating = ({rating, numReviews}) => {
             <span><i className={rating >= 3 ? "fa fa-star" : rating >= 2.5 ? "fa fa-star-half-o" : "fa fa-star-o"}></i></span>
             <span><i className={rating >= 4 ? "fa fa-star" : rating >= 3.5 ? "fa fa-star-half-o" : "fa fa-star-o"}></i></span>
             <span><i className={rating >= 5 ? "fa fa-star" : rating >= 4.5 ? "fa fa-star-half-o" : "fa fa-star-o"}></i></span>
+            <span>{` ${numReviews} reviews`}</span>
         </div>
     );
 }
