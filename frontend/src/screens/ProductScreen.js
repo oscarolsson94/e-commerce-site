34bb33c3a486bab4614c04223611ea80c1f0d82d
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-27 10:33:41 +0200

    setting up basic product page

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import Rating from '../components/Rating';
+import { data } from '../data';
 
-const ProductScreen = () => {
+const ProductScreen = (props) => {
+
+    const product = data.products.find(x => x._id === props.match.params.id);
+
+    if (!product) {
+        return <div>Product Not Found</div>
+    }
+
     return (
         <div>
-            Product Screen
+            <div className="row">
+                <div className="col-2">
+                    <img className="large" src={product.image} alt={product.name} />
+                </div>
+                <div className="col-1">
+                    <ul>
+                        <li>
+                            <h1>{product.name}</h1>
+                        </li>
+                        <li>
+                            <Rating rating={product.rating} numReviews={product.numReviews}  />
+                        </li>
+                        <li>
+                            Price : ${product.price}
+                        </li>
+                        <li>
+                            Description:
+                            <p>{product.description}</p>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col-1">
+                    
+                </div>
+            </div>
         </div>
     )
 }
