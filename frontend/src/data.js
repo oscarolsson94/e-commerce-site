c33cec11107c927427da50811fc019b81aac1457
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-27 08:42:02 +0200

    creating data file

---
+++ frontend/src/data.js
@@ -0,0 +1,70 @@
+const data = {
+    products: [
+        {
+            _id: "1",
+            name: "Nike Slim Fit Shirt",
+            category: "Shirts",
+            image: "/images/p1.jpg",
+            price: 120,
+            brand: "Nike",
+            rating: 4.5,
+            numReviews: 10,
+            description: "high quality product"
+        },
+        {
+            _id: "2",
+            name: "Adidas Shirt",
+            category: "Shirts",
+            image: "/images/p2.jpg",
+            price: 100,
+            brand: "Adidas",
+            rating: 4.0,
+            numReviews: 17,
+            description: "high quality product"
+        },
+        {
+            _id: "3",
+            name: "Lacoste Loose Shirt",
+            category: "Shirts",
+            image: "/images/p3.jpg",
+            price: 220,
+            brand: "Lacoste",
+            rating: 4.5,
+            numReviews: 10,
+            description: "high quality product"
+        },
+        {
+            _id: "4",
+            name: "Nike Slim Fit Pants",
+            category: "Pants",
+            image: "/images/p4.jpg",
+            price: 150,
+            brand: "Nike",
+            rating: 3.5,
+            numReviews: 8,
+            description: "high quality product"
+        },
+        {
+            _id: "5",
+            name: "Puma Slim Pants",
+            category: "Pants",
+            image: "/images/p5.jpg",
+            price: 65,
+            brand: "Puma",
+            rating: 4.2,
+            numReviews: 15,
+            description: "high quality product"
+        },
+        {
+            _id: "6",
+            name: "Adidas Fit Pants",
+            category: "Pants",
+            image: "/images/p6.jpg",
+            price: 55,
+            brand: "Adidas",
+            rating: 4.5,
+            numReviews: 17,
+            description: "high quality product"
+        },
+    ]
+}
