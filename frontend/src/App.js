02a660e44920111e6cc757d0ac09cb4db520475e
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-27 10:14:17 +0200

    implementing react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,38 @@
 import React from 'react';
 import Product from './components/Product';
 import { data } from './data';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+
 
 function App() {
-  return (
-    <div className="grid-container">
-        <header className="row" >
-            <div>
-                <a className="brand" href="/">Ecommerce</a>
-            </div>
-            <div>
-                <a href="/cart">Cart</a>
-                <a href="/signin">Sign In</a>
-            </div>    
-        </header>
-
-        <main>
-            <div className="row center">
-                {
-                    data.products.map(product => (
-                    <Product key={product._id} product={product} />
-                    ))      
-                }  
-            </div>
-        </main>
-        <footer className="row center" >
-            All rights reserved
-        </footer>
-</div>  
+    return (
+    <Router>  
+        <div className="grid-container">
+            <header className="row" >
+                <div>
+                    <a className="brand" href="/">Ecommerce</a>
+                </div>
+                <div>
+                    <a href="/cart">Cart</a>
+                    <a href="/signin">Sign In</a>
+                </div>    
+            </header>
+            <Route path="/product/:id" component={ProductScreen} ></Route>
+            <Route exact path="/" component={HomeScreen} ></Route>
+            <main>
+                <div className="row center">
+                    {
+                        data.products.map(product => (
+                        <Product key={product._id} product={product} />
+                        ))      
+                    }  
+                </div>
+            </main>
+            <footer className="row center" >
+                All rights reserved
+            </footer>
+        </div>
+    </Router>        
   );
 }
 
