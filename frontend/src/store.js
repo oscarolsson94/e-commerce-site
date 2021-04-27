d07965dd717a6916f171a5f5b619d1b45a8598ff
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-27 14:49:16 +0200

    dispatching productListReducer for global state

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,11 @@
-import { data } from "./data";
-import { applyMiddleware, createStore, compose } from 'redux';
+import { applyMiddleware, createStore, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
+import { productListReducer } from "./reducers/productReducers";
 
 const initialState = {};
-const reducer = (state, action) => {
-    return { products: data.products };
-}
+const reducer = combineReducers({
+    productList: productListReducer
+})
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
