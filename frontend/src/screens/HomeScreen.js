46233eba29b8557a93c7fc636b8e3647ea430112
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-27 13:15:18 +0200

    setting up loading and error handling

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -5,22 +5,39 @@ import axios from 'axios';
 const HomeScreen = () => {
 
     const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
-            const { data } = await axios.get("/api/products");
-            setProducts(data);
+            try {
+                setLoading(true);
+                const { data } = await axios.get("/api/products");
+                setLoading(false);
+                setProducts(data);
+            } catch (err) {
+                setError(err.message);
+                setLoading(false);
+            }
+            
         }
         fetchData();
     }, []);
 
     return (
-        <div className="row center">
-            {
-                products.map(product => (
-                <Product key={product._id} product={product} />
-                ))      
-            }  
+        <div>
+            {loading ? <LoadingBox />
+            :
+            error ? <MessageBox>{error}</MessageBox>
+            :
+            <div className="row center">
+                {
+                    products.map(product => (
+                    <Product key={product._id} product={product} />
+                    ))      
+                }  
+            </div>        
+            }            
         </div>
     );
 }
