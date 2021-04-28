cd564ab5a0e20e465c50bbc85416f946b6c38739
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-28 11:14:03 +0200

    link to product, select box re-use and delete btn

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -36,6 +36,35 @@ const CartScreen = (props) => {
                   <div>
                     <img src={item.image} alt={item.name} className="small" />
                   </div>
+                  <div className="min-30">
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </div>
+                  <div>
+                    <select
+                      value={item.qty}
+                      onChange={(e) =>
+                        dispatch(
+                          addToCart(item.product),
+                          Number(e.target.value)
+                        )
+                      }
+                    >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  <div>${item.price}</div>
+                  <div>
+                    <button
+                      type="button"
+                      onClick={() => removeFromCartHandler(item.product)}
+                    >
+                      Delete
+                    </button>
+                  </div>
                 </div>
               </li>
             ))}
