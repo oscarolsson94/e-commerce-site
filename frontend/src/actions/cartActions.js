2a3d84edc69a30bee28da76f31a736c650d0bd82
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-28 10:13:13 +0200

    creating cartActions and Constants

---
+++ frontend/src/actions/cartActions.js
@@ -0,0 +1,17 @@
+import { CART_ADD_ITEM } from "../constants/cartConstants";
+import axios from "axios";
+
+export const addToCart = (productId, qty) => async (dispatch, getState) => {
+  const { data } = await axios.get(`/api/products/${productId}`);
+  dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      counInStock: data.countInStock,
+      product: data._id,
+      qty,
+    },
+  });
+};
