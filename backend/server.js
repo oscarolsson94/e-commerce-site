a73d5d97a99d37a8cf232325bece6caa7496b9f8
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-28 08:33:02 +0200

    api route to get product based on id

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,27 @@
-import express from 'express';
-import { data } from './data.js';
+import express from "express";
+import { data } from "./data.js";
 
-const app = express(); 
-
+const app = express();
+//get all products
 app.get("/api/products", (req, res) => {
-    res.send(data.products);
-})
+  res.send(data.products);
+});
+//get one product
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product not Found" });
+  }
+});
 
 app.get("/", (req, res) => {
-    res.send("Server is ready");
+  res.send("Server is ready");
 });
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Serve at http://localhost:${port}`);
-})
+  console.log(`Serve at http://localhost:${port}`);
+});
