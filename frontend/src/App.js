4d4cb0ea5a68badf2ca80e802039ad261d804233
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-28 16:30:29 +0200

    styling form and adding login route to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import SigninScreen from "./screens/SigninScreen";
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -31,6 +32,7 @@ function App() {
         <main>
           <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/signin" component={SigninScreen}></Route>
           <Route exact path="/" component={HomeScreen}></Route>
         </main>
         <footer className="row center">All rights reserved</footer>
