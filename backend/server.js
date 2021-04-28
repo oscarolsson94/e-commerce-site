4b6ecbe25450c9328f353189bdc6fcb0c4470446
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-28 15:08:25 +0200

    cleaning up static data functions

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import mongoose from "mongoose";
-import { data } from "./data.js";
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
 
@@ -16,20 +15,6 @@ mongoose.connect(
     useCreateIndex: true,
   }
 );
-
-//get all products
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
-//get one product
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product not Found" });
-  }
-});
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
