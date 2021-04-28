19947d73c46a03c5c000dddfdf76ffbd4296a484
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-28 12:57:55 +0200

    inserting users from data-file to mongodb collec

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,5 +1,10 @@
 import express from "express";
+import { data } from "../data.js";
+import User from "../models/userModel";
 
 const userRouter = express.Router();
 
-userRouter.get("/seed", async (req, res) => {});
+userRouter.get("/seed", async (req, res) => {
+  const createdUsers = await User.insertMany(data.users); //inserting dummy users from data-file to collection in mongoDB
+  res.send({ createdUsers });
+});
