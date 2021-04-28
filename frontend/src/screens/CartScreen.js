960a425533fbea3225c477d12038ad7e6bdf9b76
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-28 09:02:29 +0200

    starting CartScreen & getting vars from url

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const CartScreen = (props) => {
+  const productId = props.match.params.id;
+  const qty = props.location.search
+    ? Number(props.location.search.split("=")[1])
+    : 1;
+
+  return (
+    <div>
+      <h1>Cart Screen</h1>
+    </div>
+  );
+};
+
+export default CartScreen;
