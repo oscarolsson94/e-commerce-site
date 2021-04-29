b8a330dc3d71210d3863c4a8b1e02e1e65baa5fc
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-29 11:03:04 +0200

    add shipping-route and Screen

---
+++ frontend/src/screens/ShippingAddressScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CheckoutSteps from "../components/CheckoutSteps";
+
+const ShippingAddressScreen = () => {
+  return (
+    <div>
+      <CheckoutSteps step1 step2></CheckoutSteps>
+    </div>
+  );
+};
+
+export default ShippingAddressScreen;
