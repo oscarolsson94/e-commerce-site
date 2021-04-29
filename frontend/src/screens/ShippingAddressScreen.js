91efee05f1b9c23e37465f6d379fe95f8efb2cbf
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-29 11:23:16 +0200

    setting up useState hooks for input fields

--- frontend/src/screens/ShippingAddressScreen.js
+++ frontend/src/screens/ShippingAddressScreen.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import CheckoutSteps from "../components/CheckoutSteps";
 
 const ShippingAddressScreen = () => {
+  const [fullName, setfullName] = useState("");
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [country, setCountry] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    // dispatch save shipping action
+  };
+
   return (
     <div>
       <CheckoutSteps step1 step2></CheckoutSteps>
+      <form className="form" onSubmit={submitHandler}></form>
+      <div>
+        <h1>Shipping Address</h1>
+      </div>
+      <div>
+        <label htmlFor="fullName">Full Name</label>
+        <input
+          type="text"
+          id="fullName"
+          placeholder="Enter you full name"
+          value={fullName}
+          onChange={(e) => setFullName(e.target.value)}
+          required
+        />
+      </div>
     </div>
   );
 };
