c95c095b38a938bd890c3c65aa104ab7424687b1
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-29 11:33:12 +0200

    adding the rest of the input fields

--- frontend/src/screens/ShippingAddressScreen.js
+++ frontend/src/screens/ShippingAddressScreen.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import CheckoutSteps from "../components/CheckoutSteps";
 
 const ShippingAddressScreen = () => {
-  const [fullName, setfullName] = useState("");
+  const [fullName, setFullName] = useState("");
   const [address, setAddress] = useState("");
   const [city, setCity] = useState("");
   const [postalCode, setPostalCode] = useState("");
@@ -25,9 +25,53 @@ const ShippingAddressScreen = () => {
         <input
           type="text"
           id="fullName"
-          placeholder="Enter you full name"
+          placeholder="Enter your full name"
           value={fullName}
           onChange={(e) => setFullName(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="address">Address</label>
+        <input
+          type="text"
+          id="address"
+          placeholder="Enter address"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="city">City</label>
+        <input
+          type="text"
+          id="city"
+          placeholder="Enter city"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="postalCode">Postal Code</label>
+        <input
+          type="text"
+          id="postalCode"
+          placeholder="Enter postal code"
+          value={postalCode}
+          onChange={(e) => setPostalCode(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="country">Country</label>
+        <input
+          type="text"
+          id="country"
+          placeholder="Enter country"
+          value={country}
+          onChange={(e) => setCountry(e.target.value)}
           required
         />
       </div>
