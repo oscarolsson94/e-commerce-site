46896ba51c75fdc70d7c4f08a19aa445b2d5f9c9
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-05-03 08:32:20 +0200

    fail and request action added to reducer

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -1,5 +1,7 @@
 import {
+  ORDER_CREATE_FAIL,
   ORDER_CREATE_REQUEST,
+  ORDER_CREATE_RESET,
   ORDER_CREATE_SUCCESS,
 } from "../constants/orderConstants";
 
@@ -9,6 +11,10 @@ export const orderCreateReducer = (state = {}, action) => {
       return { loading: true };
     case ORDER_CREATE_SUCCESS:
       return { loading: false, success: true, order: action.payload };
+    case ORDER_CREATE_FAIL:
+      return { loading: false, error: action.payload };
+    case ORDER_CREATE_RESET:
+      return {};
     default:
       return state;
   }
