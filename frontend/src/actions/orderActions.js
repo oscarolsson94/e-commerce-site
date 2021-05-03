9222ed07532f7bff818ed2c30c3ffac0edbffa7c
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-05-03 08:20:34 +0200

    setting up createOrder action

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,1 +1,24 @@
-export const createOrder = (order) => async (dispatch, getState) => {};
+import axios from "axios";
+import {
+  ORDER_CREATE_FAIL,
+  ORDER_CREATE_REQUEST,
+} from "../constants/orderConstants";
+
+export const createOrder = (order) => async (dispatch, getState) => {
+  dispatch({ type: ORDER_CREATE_REQUEST, payload: order });
+  try {
+    cinst { data } = await axios.post("/api/orders", order, {
+      headers: {
+      Authorization: `Bearer ${userInfo.token}`
+      },
+    })
+  } catch (error) {
+    dispatch({
+      type: ORDER_CREATE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
