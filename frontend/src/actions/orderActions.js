15bb6a793d82ea2a5d9d1289c18475b129c5c619
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-05-03 08:23:04 +0200

    getting userInfo from redux store

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -7,11 +7,14 @@ import {
 export const createOrder = (order) => async (dispatch, getState) => {
   dispatch({ type: ORDER_CREATE_REQUEST, payload: order });
   try {
-    cinst { data } = await axios.post("/api/orders", order, {
+    const {
+      userSignin: { userInfo },
+    } = getState();
+    const { data } = await axios.post("/api/orders", order, {
       headers: {
-      Authorization: `Bearer ${userInfo.token}`
+        Authorization: `Bearer ${userInfo.token}`,
       },
-    })
+    });
   } catch (error) {
     dispatch({
       type: ORDER_CREATE_FAIL,
