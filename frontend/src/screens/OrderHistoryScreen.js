8dd8d34a16434b050af725d103d169dcf31239ec
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-25 18:14:41 +0200

    route to get user info by id

--- frontend/src/screens/OrderHistoryScreen.js
+++ frontend/src/screens/OrderHistoryScreen.js
@@ -36,7 +36,7 @@ export default function OrderHistoryScreen(props) {
               <tr key={order._id}>
                 <td>{order._id}</td>
                 <td>{order.createdAt.substring(0, 10)}</td>
-                <td>{order.totalPrice}</td>
+                <td>{order.totalPrice.toFixed(2)}</td>
                 <td>{order.isPaid ? order.paidAt.substring(0, 10) : "No"}</td>
                 <td>
                   {order.isDelivered
