23da160ed1f31ee1b9fc9cdcc7e669fa8eb44c1e
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-09-25 20:18:44 +0200

    updateUser actions and constants

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { detailsUser } from "../actions/userActions";
+import { detailsUser, updateUserProfile } from "../actions/userActions";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
 
